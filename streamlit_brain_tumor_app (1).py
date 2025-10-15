1f06892755bc0c6244c231ac1d2ec13776a03c9e
Jai Ramteke <jairamteke84@gmail.com>
2025-10-15 17:53:55 +0530

    Add files via upload

---
+++ streamlit_brain_tumor_app (1).py
@@ -0,0 +1,16 @@
+# requirements.txt for AI-Powered Brain Tumor Detection Web App
+
+streamlit==1.38.0
+numpy==1.26.4
+pandas==2.2.3
+matplotlib==3.9.2
+seaborn==0.13.2
+plotly==5.24.0
+opencv-python==4.10.0.84
+Pillow==10.4.0
+tensorflow==2.17.0
+scikit-learn==1.5.2
+h5py==3.11.0
+keras==3.4.1
+scipy==1.14.1
+missingno==0.5.2
